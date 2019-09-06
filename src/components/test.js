a4d8723d827112cc9857e49b7e969930f2921de7
S.F <1074260090@qq.com>
2019-09-06 15:40:41 +0800

    first commit

---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var a = '1';
+
+function Test() {
+    return ( <div className = "App">
+        <header className = "App-header" > { a} 
+        </header> 
+        </div>
+    );
+}
+
+export default Test;
